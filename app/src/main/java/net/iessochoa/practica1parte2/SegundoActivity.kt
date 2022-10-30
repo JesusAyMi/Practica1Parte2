4618bab0e7f9b4c4fe84fefd100abf9a0596e704
JesusAyala <jesusayalamilan2@gmail.com>
2022-10-30 21:43:01 +0100

    Primera version visual

---
+++ app/src/main/java/net/iessochoa/practica1parte2/SegundoActivity.kt
@@ -0,0 +1,11 @@
+package net.iessochoa.practica1parte2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segundo)
+    }
+}
