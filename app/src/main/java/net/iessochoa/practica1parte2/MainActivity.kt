2a00e44a7d54b2ce55860d23f0fd0cc71d52e0a3
JesusAyala <jesusayalamilan2@gmail.com>
2022-10-30 22:27:18 +0100

    Parte lógica completada

--- app/src/main/java/net/iessochoa/practica1parte2/MainActivity.kt
+++ app/src/main/java/net/iessochoa/practica1parte2/MainActivity.kt
@@ -1,11 +1,45 @@
 package net.iessochoa.practica1parte2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //DEFINO LAS VARIABLES
+    private lateinit var etContraseña: EditText
+    private lateinit var btAceptar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //CONECTO LAS VARIABLES CON SUS RESPECTIVOS ELEMENTOS
+        etContraseña = findViewById(R.id.etContraseña)
+        btAceptar = findViewById(R.id.btAceptar)
+
+        //LLAMO AL MÉTODO
+        btAceptar.setOnClickListener{enviarContraseña()}
+    }
+
+    private fun enviarContraseña() {
+
+        if (!etContraseña.text.isEmpty()) {
+
+            val password: String = etContraseña.getText().toString()
+            if (password.equals("1234")){
+                val intent = Intent(this, SegundoActivity::class.java)
+                intent.putExtra(SegundoActivity.EXTRA, etContraseña.text.toString())
+                startActivity(intent)
+            } else{
+                Toast.makeText(this, getString(R.string.contraseña_incorrecta), Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.msj_contraseña), Toast.LENGTH_LONG).show()
+        }
+
     }
 }
--- app/src/main/java/net/iessochoa/practica1parte2/SegundoActivity.kt
+++ app/src/main/java/net/iessochoa/practica1parte2/SegundoActivity.kt
@@ -2,10 +2,23 @@ package net.iessochoa.practica1parte2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SegundoActivity : AppCompatActivity() {
+
+    companion object{
+        val EXTRA="practica1parte2.SegundoActivity.comtraseña"
+    }
+
+    private lateinit var tvBienvenido: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segundo)
+
+        tvBienvenido=findViewById(R.id.tvBienvenido)
+        val contraseña=intent.getStringExtra(EXTRA)
+        tvBienvenido.text="Bienvenido, la contraseña es: " + contraseña
+
     }
 }
